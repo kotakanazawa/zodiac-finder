e569a11d94674b170267dfec79e946f4acc3a187
Kota Kanazawa <kota0818@gmail.com>
2025-10-15 20:36:27 +0900

    first

---
+++ lib/zodiac/finder.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "finder/version"
+
+module Zodiac
+  module Finder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/zodiac/finder/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Zodiac
+  module Finder
+    VERSION = "0.1.0"
+  end
+end
